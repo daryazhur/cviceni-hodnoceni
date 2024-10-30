293d3aad22c469e6ad3c6e1453fb538ada97d384
daryazhur <daryazhur@tut.by>
2024-10-30 20:20:32 +0100

    reseni

--- src/components/Star/index.jsx
+++ src/components/Star/index.jsx
@@ -1,14 +1,19 @@
 import { useState } from 'react';
 import './style.css';
 
-export const Star = () => {
-  const [glowing, setGlowing] = useState(false);
+export const Star = ({ value, onSelect, glowing }) => {
+  // const [glowing, setGlowing] = useState(false);
   
   const starClass = glowing
     ? 'rating__star rating__star--on'
     : 'rating__star';
 
+  // const handleClick = () => {
+  //   glowing(true)
+  //   onSelect(value)
+  // }
+
   return (
-    <div className={starClass}></div>
+    <div className={starClass} onClick={() => {onSelect(value)}}></div>
   );
 };
